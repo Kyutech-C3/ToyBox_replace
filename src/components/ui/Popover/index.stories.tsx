eb474a5a97c05494daf5402aab72e1e2cbbb238b
siloneco <siloneco4646@gmail.com>
2023-11-10 06:40:50 +0000

    feat: add align previews and radio button

--- src/components/ui/Popover/index.stories.tsx
+++ src/components/ui/Popover/index.stories.tsx
@@ -6,23 +6,36 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { cn } from '@/libs/utils';
 
-const meta: Meta<typeof Popover> = {
+type PopoverPropsAndCustomArgs = React.ComponentProps<typeof Popover> & {
+  align?: 'center' | 'start' | 'end';
+};
+
+const meta: Meta<PopoverPropsAndCustomArgs> = {
   component: Popover,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    align: {
+      options: ['center', 'start', 'end'],
+      control: { type: 'radio' },
+    },
+  },
 };
 
 export default meta;
 
-type Story = StoryObj<typeof Popover>;
+type Story = StoryObj<PopoverPropsAndCustomArgs>;
 
 export const Default: Story = {
-  render: () => (
+  args: {
+    align: 'start',
+  },
+  render: (args) => (
     <Popover>
       <PopoverTrigger>Popover</PopoverTrigger>
-      <PopoverContent>Content</PopoverContent>
+      <PopoverContent {...args}>Content</PopoverContent>
     </Popover>
   ),
 };
@@ -30,6 +43,67 @@ export const Default: Story = {
 export const WithButton: Story = {
   render: () => (
     <Popover>
+      <PopoverTrigger asChild>
+        <Button>Popover</Button>
+      </PopoverTrigger>
+      <PopoverContent className={cn('text-white', 'bg-black')}>
+        Content
+      </PopoverContent>
+    </Popover>
+  ),
+};
+
+export const AlignStart: Story = {
+  args: {
+    align: 'start',
+  },
+  render: (args) => (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button>Popover</Button>
+      </PopoverTrigger>
+      <PopoverContent {...args} className={cn('text-white', 'bg-black')}>
+        Content
+      </PopoverContent>
+    </Popover>
+  ),
+};
+
+export const AlignCenter: Story = {
+  args: {
+    align: 'center',
+  },
+  render: (args) => (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button>Popover</Button>
+      </PopoverTrigger>
+      <PopoverContent {...args} className={cn('text-white', 'bg-black')}>
+        Content
+      </PopoverContent>
+    </Popover>
+  ),
+};
+
+export const AlignEnd: Story = {
+  args: {
+    align: 'end',
+  },
+  render: (args) => (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button>Popover</Button>
+      </PopoverTrigger>
+      <PopoverContent {...args} className={cn('text-white', 'bg-black')}>
+        Content
+      </PopoverContent>
+    </Popover>
+  ),
+};
+
+export const Opened: Story = {
+  render: () => (
+    <Popover open>
       <PopoverTrigger asChild>
         <Button>Popover</Button>
       </PopoverTrigger>
