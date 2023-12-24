646ddca4a4d6601161deb7d27bd9892c427bb97d
siloneco <siloneco4646@gmail.com>
2023-12-24 16:24:12 +0000

    test: add search params test for url validation

--- src/components/model/Work/components/URLInput/logics/index.test.ts
+++ src/components/model/Work/components/URLInput/logics/index.test.ts
@@ -5,29 +5,34 @@ describe('isValidUrl', () => {
     const url = 'https://example.com';
     expect(isValidUrl(url)).toBe(true);
     expect(isValidUrl(`${url}/`)).toBe(true);
+    expect(isValidUrl(`${url}/?foo=bar`)).toBe(true);
   });
 
   it('returns false when its called with invalid string', () => {
     const notUrl = 'foobar';
     expect(isValidUrl(notUrl)).toBe(false);
     expect(isValidUrl(`${notUrl}/`)).toBe(false);
+    expect(isValidUrl(`${notUrl}/?foo=bar`)).toBe(false);
   });
 
   it('returns false when its called with only protocol', () => {
     const protocol = 'https://';
     expect(isValidUrl(protocol)).toBe(false);
     expect(isValidUrl(`${protocol}/`)).toBe(false);
+    expect(isValidUrl(`${protocol}/?foo=bar`)).toBe(false);
   });
 
   it('returns false when its called with non-alphabetic hostname', () => {
     const susUrl = 'https://ーーー';
     expect(isValidUrl(susUrl)).toBe(false);
     expect(isValidUrl(`${susUrl}/`)).toBe(false);
+    expect(isValidUrl(`${susUrl}/?foo=bar`)).toBe(false);
   });
 
   it('returns false when its called with hostname without dot', () => {
     const susUrl = 'https://hostname/';
     expect(isValidUrl(susUrl)).toBe(false);
     expect(isValidUrl(`${susUrl}/`)).toBe(false);
+    expect(isValidUrl(`${susUrl}/?foo=bar`)).toBe(false);
   });
 });
