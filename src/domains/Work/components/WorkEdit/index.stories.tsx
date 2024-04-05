11cefbd4e56e31f420afa73885953d50c57c43c9
tosaken1116 <nagato20021122@gmail.com>
2024-04-05 21:57:08 +0900

    chore: add work edit mock data

--- src/domains/Work/components/WorkEdit/index.stories.tsx
+++ src/domains/Work/components/WorkEdit/index.stories.tsx
@@ -15,5 +15,108 @@ export default meta;
 type Story = StoryObj<typeof WorkEditPresentation>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    assets: [
+      {
+        id: '1',
+        url: 'https://placehold.jp/150x150.png',
+        assetType: 'image',
+        extension: 'png',
+      },
+    ],
+    errors: {
+      title: {
+        type: 'required',
+        message: '',
+      },
+      description: {
+        type: 'required',
+        message: '',
+      },
+    },
+    links: ['https://example.com'],
+    registers: {
+      title: {
+        onChange: function (): Promise<boolean | void> {
+          return new Promise(() => {});
+        },
+        onBlur: function (): Promise<boolean | void> {
+          return new Promise(() => {});
+        },
+        ref: function (): void {},
+        name: 'title',
+      },
+      description: {
+        onChange: function (): Promise<boolean | void> {
+          return new Promise(() => {});
+        },
+        onBlur: function (): Promise<boolean | void> {
+          return new Promise(() => {});
+        },
+        ref: function (): void {},
+        name: 'description',
+      },
+    },
+    handleChangeCursor: () => {},
+    handleChangePostDiscord: () => {},
+    handleChangeVisibility: () => {},
+    handleEditDescription: () => {},
+    handlePublish: async () =>
+      await new Promise(() => ({
+        id: '1',
+      })),
+    handleSetLinks: () => {},
+    handleSetTag: () => {},
+    handleUploadAssets: async () => {},
+    previewWork: {
+      title: '作品タイトル',
+      thumbnailUrl: 'https://placehold.jp/150x150.png',
+      tags: [
+        {
+          id: '1',
+          name: 'タグ名',
+          color: 'red',
+          textColor: 'white',
+        },
+      ],
+      creator: {
+        id: '1',
+        displayName: 'ユーザー名',
+        avatarUrl: 'https://placehold.jp/150x150.png',
+      },
+      createdAt: '2022-01-01',
+      isPublic: true,
+    },
+    defaultWork: {
+      title: '作品タイトル',
+      description: '作品説明',
+      thumbnail: {
+        id: '1',
+        url: 'https://placehold.jp/150x150.png',
+      },
+      tags: [
+        {
+          id: '1',
+          name: 'タグ名',
+          color: 'red',
+          textColor: 'white',
+        },
+      ],
+      assets: [
+        {
+          id: '1',
+          url: 'https://placehold.jp/150x150.png',
+          assetType: 'image',
+          extension: 'png',
+        },
+      ],
+      urls: [
+        {
+          url: 'https://example.com',
+          urlType: 'others',
+        },
+      ],
+      isPublic: true,
+    },
+  },
 };
