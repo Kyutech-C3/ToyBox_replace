da611784b8ce3cc0be096ed170f7d5686e0e78b6
tosaken1116 <nagato20021122@gmail.com>
2024-04-09 00:01:22 +0900

    feat: add story mock data

--- src/components/ui/MarkdownViewer/index.stories.tsx
+++ src/components/ui/MarkdownViewer/index.stories.tsx
@@ -15,5 +15,48 @@ export default meta;
 type Story = StoryObj<typeof MarkdownViewer>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    children: `
+# Title
+## Subtitle
+### h3
+#### h4
+##### h5
+###### h6
+
+- list1
+- list2
+- list3
+
+1. list1
+2. list2
+3. list3
+
+\`\`\`js
+console.log('Hello, World!');
+\`\`\`
+
+\`\`\`css
+body {
+  background-color: #fff;
+}
+\`\`\`
+
+
+\`inline code\`
+
+> blockquote
+
+[link](https://google.com)
+
+![image](https://avatars.githubusercontent.com/u/1?v=4)
+
+**bold**
+
+*italic*
+
+---
+
+`,
+  },
 };
