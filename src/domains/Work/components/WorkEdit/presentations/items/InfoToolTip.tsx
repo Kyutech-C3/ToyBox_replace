f5c7684cc654a9b1bbc79d42473fcb6162042822
powaaaaa <knyuki2525@gmail.com>
2024-05-07 00:47:33 +0900

    feat: information tool tip

---
+++ src/domains/Work/components/WorkEdit/presentations/items/InfoToolTip.tsx
@@ -0,0 +1,27 @@
+import type { FC, ReactNode } from 'react';
+
+import { Info } from 'lucide-react';
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/Tooltip';
+
+type Props = {
+  children: ReactNode;
+};
+
+export const InfoToolTip: FC<Props> = ({ children }) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <Info size={16} color="#757575" />
+      </TooltipTrigger>
+      <TooltipContent className="rounded-md relative text-xl border-2 bg-white border-orange-pop">
+        <p className="whitespace-pre-wrap text-[10px] font-bold">{children}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
