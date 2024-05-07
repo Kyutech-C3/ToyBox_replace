611ecf8219568fa3df4b0ec47803239097b90b81
powaaaaa <knyuki2525@gmail.com>
2024-05-07 21:15:22 +0900

    refactor: add supportExtension in constants

---
+++ src/constants/supportExtension/asset.ts
@@ -0,0 +1,22 @@
+export const ASSET_EXTENSION = [
+  {
+    category: '画像',
+    exts: ['png', 'jpg', 'jpeg', 'bmp', 'gif'],
+  },
+  {
+    category: '動画',
+    exts: ['mp4', 'mov'],
+  },
+  {
+    category: '音源',
+    exts: ['mp3', 'wav', 'm4a'],
+  },
+  {
+    category: 'モデル',
+    exts: ['gltf', 'fbx'],
+  },
+  {
+    category: 'zip',
+    exts: ['zip'],
+  },
+];
---
+++ src/constants/supportExtension/thumbnail.ts
@@ -0,0 +1,6 @@
+export const THUMBNAIL_EXTENSION = [
+  {
+    category: '画像',
+    exts: ['png', 'jpg', 'jpeg', 'bmp', 'gif'],
+  },
+];
